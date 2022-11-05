fc76344143e71c309f150bef8f36ad0a472c0f7c
David Gageot <david.gageot@docker.com>
2022-11-05 11:27:32 +0100

    Update minimo theme
    Signed-off-by: David Gageot <david.gageot@docker.com>

--- themes/minimo/exampleSite/content/docs/shortcodes.md
+++ themes/minimo/exampleSite/content/docs/shortcodes.md
@@ -1,5 +1,6 @@
 ---
 date: 2017-10-05T20:00:00+06:00
+lastmod: 2020-10-06T02:00:00+06:00
 title: Shortcodes
 authors: ["muniftanjim"]
 categories:
@@ -22,17 +23,58 @@ Center align you content.
 - Markdown content between opening and closing tags.
 
 ### center: Usage Example
+
 ```golang
-{{%/* center */%}}
+{{</* center */>}}
 _Center Aligned Text_
-{{%/* /center */%}}
+{{</* /center */>}}
 ```
 
-**Output**
+**Output**:
 
-{{% center %}}
+{{< center >}}
 _Center Aligned Text_
-{{% /center %}}
+{{< /center >}}
+
+-------
+
+## Shortcode: convo
+
+Renders conversation blocks.
+
+### convo: Parameters
+
+- `sep`  [`String`] \(optional\): seperator between person and text (default: "`:`")
+
+### convo: Inner Syntax
+
+```golang
+person :: text
+```
+
+_You can remove the **`person`** part, if you want._
+
+### convo: Usage Example
+
+```golang
+{{</* convo sep=":" */>}}
+
+Jerry :: You don't look so tough.
+
+Finch :: It's because I have only two modes, Jerry. Calm, and furious. It's a rare person that sees the latter and lives to talk about it.
+
+{{</* /convo */>}}
+```
+
+**Output**:
+
+{{< convo sep=":" >}}
+
+Jerry :: You don't look so tough.
+
+Finch :: It's because I have only two modes, Jerry. Calm, and furious. It's a rare person that sees the latter and lives to talk about it.
+
+{{< /convo >}}
 
 -------
 
@@ -51,11 +93,41 @@ Include content from seperate file with syntax highlighting.
 {{</* file "content/_index.md" */>}}
 ```
 
-**Output**
+**Output**:
 
 {{< file "content/_index.md" >}}
 
----
+-------
+
+## Shortcode: katex
+
+Renders KaTeX.
+
+### katex: Parameters
+
+0 => mode [`String`] \(optional\): inline / block
+
+### katex: Usage Example
+
+```golang
+Inline: {{</* katex inline */>}} ax^2 + bx + c = 0 {{</* /katex */>}}
+
+Block:
+{{</* katex */>}}
+x = {-b \pm \sqrt{b^2-4ac} \over 2a}
+{{</* /katex */>}}
+```
+
+**Output**:
+
+Inline: {{< katex inline >}} ax^2 + bx + c = 0 {{< /katex >}}
+
+Block:
+{{< katex >}}
+x = {-b \pm \sqrt{b^2-4ac} \over 2a}
+{{< /katex >}}
+
+-------
 
 ## Shortcode: text
 
@@ -65,12 +137,12 @@ Text with custom size and color
 
 You can use either Named or Unnamed Parameters
 
-**Named Parameters**
+**Named Parameters**:
 
 - `s` or `size`  [`String`] \(optional\): multiplier relative to the normal size
 - `c` or `color` [`String`] \(optional\): name / hex / rgb / rgba
 
-**Unnamed Parameters**
+**Unnamed Parameters**:
 
 0 => textsize [`String`] \(required\): multiplier relative to the normal size  
 1 => textcolor [`String`] \(optional\): name / hex / rgb / rgba
@@ -78,17 +150,37 @@ You can use either Named or Unnamed Parameters
 ### text: Usage Example
 
 ```golang
-{{%/* text s="1.4" color="purple" */%}}
+{{</* text s="1.4" color="purple" */>}}
 font-size: 1.4em;
 color: purple;
-{{%/* /text */%}}
+{{</* /text */>}}
 ```
 
-**Output**
+**Output**:
 
-{{% text s="1.4" color="purple" %}}
+{{< text s="1.4" color="purple" >}}
 font-size: 1.5em;  
 color: purple;
-{{% /text %}}
+{{< /text >}}
 
--------
+
+## Shortcode: slideshare
+
+As default Hugo's shortcodes for Twitter, Instagram, Vimeo, Youtube you can embed [SlideShare](https://www.slideshare.net/) presentation/document into your page.
+
+### slideshare: Parameters
+
+**Named Parameters**:
+
+- `id` [`String`] \(required\): `id` of SlideShare you want to embed. You can find it in
+SlideShare URL or click "Share" -> See `id` field in "WordPress Shortcode" section.
+
+### slideshare: Usage Example
+
+```golang
+{{</* slideshare 237760779 */>}}
+```
+
+**Output**:
+
+{{< slideshare id="237760779" >}}
--- themes/minimo/webpack.config.js
+++ themes/minimo/webpack.config.js
@@ -1,22 +1,11 @@
-const webpack = require('webpack')
 const path = require('path')
 const autoprefixer = require('autoprefixer')
-const AssetsPlugin = require('assets-webpack-plugin')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const cssnano = require('cssnano')
+const AssetsWebpackPlugin = require('assets-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
 
-const define = new webpack.DefinePlugin({
-  'process.env': {
-    NODE_ENV: JSON.stringify(process.env.NODE_ENV)
-  }
-})
-
-const extractCSS = new ExtractTextPlugin({
-  filename: getPath =>
-    getPath('css/[name].[contenthash:8].css').replace('css', '../css')
-})
-
-const assetsManifest = new AssetsPlugin({
+const assetsManifest = new AssetsWebpackPlugin({
   filename: 'assets.json',
   path: path.join(__dirname, 'data'),
   fullPath: false,
@@ -31,18 +20,32 @@ const assetsManifest = new AssetsPlugin({
   }
 })
 
-const cleanBuild = new CleanWebpackPlugin([
-  'static/assets/css/*',
-  'static/assets/js/*'
-])
+const extractCSS = new MiniCSSExtractPlugin({
+  filename: '../css/[name].[contenthash:8].css'
+})
+
+const cleanBuild = new CleanWebpackPlugin({
+  cleanOnceBeforeBuildPatterns: [
+    path.resolve('static/assets/css/*'),
+    path.resolve('static/assets/js/*')
+  ]
+})
+
+const node_env = process.env.NODE_ENV
 
 const config = {
+  mode: node_env === 'production' ? 'production' : 'development',
   entry: {
-    main: path.join(__dirname, 'src/scripts', 'main.js')
+    main: path.join(__dirname, 'src/scripts', 'main.js'),
+    algolia_search: path.join(__dirname, 'src/scripts/search', 'algolia.js'),
+    fuse_search: path.join(__dirname, 'src/scripts/search', 'fuse.js'),
+    lunr_search: path.join(__dirname, 'src/scripts/search', 'lunr.js'),
+    rtl: path.join(__dirname, 'src/stylesheets', 'rtl.scss')
   },
   output: {
     filename: '[name].[chunkhash:8].js',
-    path: path.join(__dirname, 'static', 'assets', 'js')
+    chunkFilename: '[name].[chunkhash:8].js',
+    path: path.join(__dirname, 'static', 'assets/js')
   },
   module: {
     rules: [
@@ -52,43 +55,49 @@ const config = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['es2015']
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-syntax-dynamic-import']
           }
         }
       },
       {
         test: /\.scss$/,
-        include: path.resolve(__dirname, 'src/stylesheets'),
-        loader: extractCSS.extract({
-          use: [
-            {
-              loader: 'css-loader',
-              options: {
-                importLoaders: 1
-              }
-            },
-            {
-              loader: 'postcss-loader',
-              options: {
-                plugins: [
-                  autoprefixer({
-                    browsers: ['> 1%', 'last 2 versions']
-                  })
-                ]
-              }
-            },
-            'sass-loader'
-          ]
-        })
+        include: [path.resolve(__dirname, 'src/stylesheets')],
+        use: [
+          MiniCSSExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [
+                autoprefixer(),
+                'production' === node_env
+                  ? cssnano({
+                      preset: [
+                        'default',
+                        { discardComments: { removeAllButFirst: true } }
+                      ]
+                    })
+                  : null
+              ].filter(Boolean)
+            }
+          },
+          'sass-loader'
+        ]
       }
     ]
   },
   resolve: {
     extensions: ['*', '.js', '.scss']
   },
-  plugins: [define, extractCSS, assetsManifest]
+  plugins: [extractCSS, assetsManifest]
 }
 
-if (process.env.NODE_ENV === 'production') config.plugins.push(cleanBuild)
+if (node_env === 'production') config.plugins.push(cleanBuild)
 
 module.exports = config
